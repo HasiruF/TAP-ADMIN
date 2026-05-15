3e876fb3e8f76d757f8da4b9c5feb2dc798573f7
Hasiru Fonseka <hasiru050803@gmail.com>
2026-05-15 10:55:20 +0530

    logo fix

--- src/components/admin/layout/SideBar.tsx
+++ src/components/admin/layout/SideBar.tsx
@@ -73,7 +73,7 @@ export function AppSidebar() {
                 }}
               >
                 <img
-                  src="/primary.svg"
+                  src="/Primary.svg"
                   alt="TAP Logo"
                   className="w-12 h-12 object-contain"
                 />
