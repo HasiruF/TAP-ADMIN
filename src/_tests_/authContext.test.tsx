8230e6e33e21c65c0d958ed1ceee907c520aa29a
Hasiru Fonseka <hasiru050803@gmail.com>
2026-06-24 15:57:07 +0530

    logout test

--- src/_tests_/authContext.test.tsx
+++ src/_tests_/authContext.test.tsx
@@ -1,10 +1,13 @@
-import { render, waitFor } from '@testing-library/react'
-import { AuthProvider } from '@/lib/api/auth/AuthContext'
+import { render, waitFor, renderHook, act } from '@testing-library/react'
+import { AuthProvider, useAuthContext } from '@/lib/api/auth/AuthContext'
 import * as auth from '@/lib/api/auth'
 
 import '@testing-library/jest-dom'
 
-jest.mock('@/lib/api/auth')
+jest.mock('@/lib/api/auth', () => ({
+  logout: jest.fn(),
+  refresh: jest.fn(),
+}))
 
 describe('AuthContext initialization', () => {
   beforeEach(() => {
@@ -29,6 +32,34 @@ describe('AuthContext initialization', () => {
 
     await waitFor(() => {
       expect(auth.refresh).toHaveBeenCalled()
+    })
+  })
+  describe('AuthContext logout', () => {
+    beforeEach(() => {
+      jest.clearAllMocks()
+      localStorage.clear()
+    })
+
+    it('clears all auth state on logout', async () => {
+      jest
+        .spyOn(auth, 'refresh')
+        .mockRejectedValue(new Error('No refresh token'))
+
+      const { result } = renderHook(() => useAuthContext(), {
+        wrapper: AuthProvider,
+      })
+
+      await act(async () => {
+        await result.current.logout()
+      })
+
+      expect(localStorage.getItem('tap_refresh_token')).toBeNull()
+
+      expect(result.current.user).toBeNull()
+
+      expect(result.current.isAuthenticated).toBe(false)
+
+      expect(result.current.isLoading).toBe(false)
     })
   })
 })
